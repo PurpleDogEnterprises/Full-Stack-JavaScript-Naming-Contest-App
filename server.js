b6fd6417ca60815f8a16693bca92645d8cb74952
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2016-11-30 12:11:17 -0500

    Added Sass

--- server.js
+++ server.js
@@ -1,9 +1,3 @@
-import config from './config';
-import apiRouter from './api';
-
-import express from 'express';
-const server = express();
-
 server.set('view engine', 'ejs');
 
 server.get('/', (req, res) => {
