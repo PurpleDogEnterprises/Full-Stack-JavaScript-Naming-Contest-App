4e9a82fd9f0959244330acf08346671ad0f34eec
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2016-12-02 09:33:31 -0500

    Added contest lookup on route change

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import data from '../src/testData';
 
 const router = express.Router();
+const contests = data.contests.reduce((obj, contest) => {
+  obj[contest.id] = contest;
+  return obj;
+}, {});
 
-router.get('/', (req, res) => {
-  res.send({ data: [] });
+router.get('/contests', (req, res) => {
+  res.send({
+    contests: contests
+  });
 });
 
 export default router;
